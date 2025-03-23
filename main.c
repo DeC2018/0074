2b937214cf8995c358b2d2c1f934504bf90c3616
Den <37459110+DeC2018@users.noreply.github.com>
2025-03-24 05:09:11 +1000

    Update main.c

--- main.c
+++ main.c
@@ -1,41 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 
 bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target) {
-    int left = 0;
-    int right = matrixSize * (*matrixColSize) - 1;
-
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        int midValue = matrix[mid / (*matrixColSize)][mid % (*matrixColSize)];
-
-        if (midValue == target) {
+    int r = 0;
+    int c = *matrixColSize - 1;
+    while (r < matrixSize && c >= 0) {
+        if (target == matrix[r][c])
             return true;
-        } else if (midValue < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
+        else if (matrix[r][c] > target)
+            c--;
+        else
+            r++;
     }
-
     return false;
 }
 
 int main() {
-    int matrix1[3][4] = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
-    int* matrix1Ptrs[3] = {matrix1[0], matrix1[1], matrix1[2]};
-    int matrix1ColSize = 4;
-    int target1 = 3;
-    printf("Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = %d\n", target1);
-    printf("Output: %s\n", searchMatrix(matrix1Ptrs, 3, &matrix1ColSize, target1) ? "true" : "false");
+    int m1[] = {1,3,5,7};
+    int m2[] = {10,11,16,20};
+    int m3[] = {23,30,34,60};
+    int* matrix[] = {m1, m2, m3};
+    int matrixColSize = 4;
 
-    int matrix2[3][4] = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
-    int* matrix2Ptrs[3] = {matrix2[0], matrix2[1], matrix2[2]};
-    int matrix2ColSize = 4;
+    int target1 = 3;
+    printf("Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3\nOutput: %s\n", searchMatrix(matrix, 3, &matrixColSize, target1) ? "true" : "false");
+
     int target2 = 13;
-    printf("Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = %d\n", target2);
-    printf("Output: %s\n", searchMatrix(matrix2Ptrs, 3, &matrix2ColSize, target2) ? "true" : "false");
+    printf("Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13\nOutput: %s\n", searchMatrix(matrix, 3, &matrixColSize, target2) ? "true" : "false");
 
     return 0;
 }
